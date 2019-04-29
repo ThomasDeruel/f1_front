4612c1f29d23d5ec57063816732f1e01872b725f
ThomasFranja <thomas.franja@hetic.net>
2019-04-29 10:19:30 +0200

    home section pilots created + add assets + btn slider created + home & btn scss modified

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import MenuConsumer from '../../components/MenuConsumer';
+
+// IMPORT IMG
 import logo from '../../styles/images/logo.png';
 import flesh from '../../styles/images/flesh.png';
 import galery from '../../styles/images/galery.png';
@@ -9,9 +10,19 @@ import Section1 from '../../styles/images/Section1.png';
 import graph1 from '../../styles/images/f1-h-section3-bg-graph01.png';
 import graph2 from '../../styles/images/f1-h-section3-bg-graph02.png';
 import bgEcurie from '../../styles/images/f1-h-section3-img-ecurie.png';
+import pilots01 from '../../styles/images/f1-h-section4-bg-perso01.jpg';
+import pilots02 from '../../styles/images/f1-h-section4-bg-perso02.jpg';
+import pilots03 from '../../styles/images/f1-h-section4-bg-perso03.jpg';
+
+// IMPORT STYLES
 import '../../styles/scss/home.scss';
+
+// IMPORT COMPONENTS
+import MenuConsumer from '../../components/MenuConsumer';
 import TitleDataVize from '../../components/HomeComponent.js/titleDataVize';
 import BtnHome from '../../components/Btn/Btn';
+import BtnSlider from '../../components/Btn/BtnSlider';
+
 const HomeContainer = ({ menuOpen, setMenuOpen }) => {
 
   return (
@@ -76,7 +87,7 @@ const HomeContainer = ({ menuOpen, setMenuOpen }) => {
             <div className={'container-second'}>
                 <div className={'container-second_description'}>
                     <h2 className={'title'}>Les Ecuries</h2>
-                    <h3>Parce que la f1 c'est une équipe</h3>
+                    <h3 className={'sub-title'}>Parce que la f1 c'est une équipe</h3>
                     <p>There are many simple beauty tips that don’t have to take up a lot of time.
                         These simple tips can be accomplished in a matter of minutes and can have a noticeable improvement on your appearance.
                         These beauty tips include simple suggestions such as getting enough sleep each night, drinking plenty of water each day and
@@ -88,6 +99,30 @@ const HomeContainer = ({ menuOpen, setMenuOpen }) => {
             </div>
         </div>
       </section>
+        {/* SECTION PILOTES */}
+        <section className="section4">
+            <div className="inner">
+                <div className="container-title">
+                    <h2 className={'title'}>Les Pilotes</h2>
+                    <h3 className={'sub-title'}>Ils ont marqués leur sport</h3>
+                </div>
+                <div className="container-pilots">
+                    <div className="container-pilots_description">
+                        <h2 className="container-pilots_name">Ashely Right</h2>
+                        <span className="container-pilots_constructor">Porsche</span>
+                        <p className="container-pilots_text">"Proffesionals in their craft! All products were super
+                            amazing with strong attention to details, comps. and overall vibe"
+                        </p>
+                    </div>
+                    <BtnSlider/>
+                    <div className={'container-pilots_bg'}>
+                        <img src={pilots01} alt="bg-pilots-01"/>
+                        <img src={pilots02} alt="bg-pilots-02"/>
+                        <img src={pilots03} alt="bg-pilots-03"/>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
   )
 }
